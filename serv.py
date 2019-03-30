53f6360b5910287388bc6439abbfa67c7d7d11eb
Laicell <ilya.laical@yandex.ru>
2019-03-30 12:35:10 +0300

    Сайт и ссылки!

--- serv.py
+++ serv.py
@@ -1,17 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Charlie Shuffler',
+        'title': 'Post 1',
+        'content': 'Первый пост',
+        'date_posted': 'Март 20, 2019'
+    },
+    {
+        'author': 'Jane Logan',
+        'title': 'Post 2',
+        'content': 'Второй пост',
+        'date_posted': 'Март 21, 2019'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello World!)</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
